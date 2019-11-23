64b4ea2f8bf3f04aa69dc70a6f2909a7bea52370
Naima <nbensassi@hotmail.com>
2019-11-22 21:50:21 -0500

    Good to go on files

--- liri.js
+++ liri.js
@@ -9,17 +9,19 @@ var axios = require("axios");
 var comand = process.argv[2];
 var secondComand = process.argv[3];
 
+let search = process.argv.slice(3).join(" ");
+console.log(typeof (search))
 switch (comand) {
     case "concert-this":
-        concertThis();
+        concertThis(search);
         break;
 
     case "spotify-this-song":
-        spotifyThisSong();
+        spotifyThisSong(search);
         break;
 
     case "movie-this":
-        movieGet();
+        movieGet(secondComand);
         break;
 
     case "do-what-it-says":
@@ -28,70 +30,73 @@ switch (comand) {
 
 }
 
-function movieGet() {
-    var movieName = secondComand;
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+function movieGet(search) {
+    if (!search) {
+        console.log("-----------------------");
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+        console.log("It's on Netflix!");
+    } else {
+        let movieName = search
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-    console.log(queryUrl);
-    axios.get(queryUrl).then(
-        function (response) {
-            console.log("===================Movie Info====================")
-            console.log("Title of the movie: " + movieName);
-            console.log("Release Year: " + response.data.Year);
-            console.log("IMDB Rating: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-            console.log("Country: " + response.data.Country);
-            console.log("Language: " + response.data.Language);
-            console.log("Plot: " + response.data.Plot);
-            console.log("Actors: " + response.data.Actors);
-            console.log("=====================End Movie Info===============")
+        console.log(queryUrl);
+        axios.get(queryUrl).then(
+            function (response) {
+                console.log("===================Movie Info====================")
+                console.log("Title of the movie: " + movieName);
+                console.log("Release Year: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+                console.log("=====================End Movie Info===============")
 
-        })
-        .catch(function (error) {
-            if (movieName === "Mr.Nobody") {
-                console.log("-----------------------");
-                console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-                console.log("It's on Netflix!");
-            }
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log("---------------Data---------------");
-                console.log(error.response.data);
-                console.log("---------------Status---------------");
-                console.log(error.response.status);
-                console.log("---------------Status---------------");
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
-            }
-            console.log(error.config);
+            })
+            .catch(function (error) {
 
-        });
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+
+            });
+    }
 
 }
 
 //band in Town function
-function concertThis() {
-    let artist = secondComand;
-
+function concertThis(search) {
+    let artist = search;
+    console.log(artist)
     moment().format();
-
-
     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     // var queryUrl = "https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp";
 
     axios.get(queryUrl).then(
         function (response) {
-            console.log("Name of The Vanue: " + response.data[0].venue.name);
-            console.log("Venue location: " + response.data[0].venue.city);
-            console.log("Venue Date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
-
+            if (response.data.length) {
+                console.log("Name of The Vanue: " + response.data[0].venue.name);
+                console.log("Venue location: " + response.data[0].venue.city);
+                console.log("Venue Date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+            } else {
+                console.log(`It would appear as though ${search} is not on tour.`)
+            }
         })
         .catch(function (error) {
             if (error.response) {
@@ -126,9 +131,12 @@ function concertThis() {
 
 
 function spotifyThisSong(song) {
+    // Check if the command is there. If it is not, use the sign
+    if (!song) {
+        song = "The Sign"
+    }
 
     spotify.search({ type: 'track', query: song, limit: 1 }, function (err, data) {
-
 
         for (var i = 0; i < data.tracks.items.length; i++) {
             var songData = data.tracks.items[i];
@@ -140,11 +148,8 @@ function spotifyThisSong(song) {
 
         }
 
-        if (secondCommand) {
-            spotifyThisSong(secondCommand);
-        } else {
-            spotifyThisSong("The Sign");
-        }
+
+
         if (err) {
             return console.log('Error occurred: ' + err);
         }
